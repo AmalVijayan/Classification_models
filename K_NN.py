d5bd0cfb7ffa8e83348b74014438c07284fffe7b
AmalVijayan <amalvnair95@gmail.com>
2018-11-09 22:27:01 +0530

    Committing Python program for K-NN  Classification

---
+++ K_NN.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  9 21:26:30 2018
+
+@author: amal
+"""
+
+#importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#importing dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+
+"""classifying dependent and independent variables
+   here country, age and salary are independent variables
+   where as purchased is dependent"""
+
+X = dataset.iloc[:,2:4].values  
+Y = dataset.iloc[:,4].values  
+
+"""
+#Dealing with missing data
+
+from sklearn.preprocessing import Imputer
+
+imputer = Imputer(missing_values='NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
+#df_X = pd.DataFrame(X) 
+
+
+#Dealing with categorical data
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+labelencoder_Y = LabelEncoder()
+Y=labelencoder_Y.fit_transform(Y)
+"""
+
+
+#splitting into training set and test set
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train,Y_test = train_test_split(X,Y,test_size = 0.25, random_state=0)
+
+
+#Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test) 
+
+
+########################################### KNN #######################################
+
+#create a aclassifier and provide the training set 
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski')
+classifier.fit(X_train,Y_train)
+
+#predict the test set results
+
+y_pred = classifier.predict(X_test)
+
+#confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test,y_pred)
+
+#visualising training set results
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_train,Y_train
+X1,X2 = np.meshgrid(np.arange(start = X_set[:,0].min()-1, stop = X_set[:,0].max()+1, step = 0.01),
+                    np.arange(start = X_set[:,1].min()-1, stop = X_set[:,1].max()+1, step = 0.01))
+
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.50, cmap = ListedColormap(('red','green')))
+
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+
+for i,j in enumerate(np.unique(Y_set)):
+     plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c = ListedColormap(('red','green'))(i), label = j)
+    
+plt.title('KNN (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+#visualising training set results
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_test,Y_test
+X1,X2 = np.meshgrid(np.arange(start = X_set[:,0].min()-1, stop = X_set[:,0].max()+1, step = 0.01),
+                    np.arange(start = X_set[:,1].min()-1, stop = X_set[:,1].max()+1, step = 0.01))
+
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.50, cmap = ListedColormap(('red','green')))
+
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+
+for i,j in enumerate(np.unique(Y_set)):
+     plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c = ListedColormap(('red','green'))(i), label = j)
+    
+plt.title('KNN (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
