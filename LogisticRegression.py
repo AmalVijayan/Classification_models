1e0d9791652fac0dd5fa32c3e5266be62a5c2494
AmalVijayan <amalvnair95@gmail.com>
2018-11-09 22:26:46 +0530

    Committing Python program for Logistic  Regression Classification

---
+++ LogisticRegression.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  9 20:20:25 2018
+
+@author: amal
+"""
+
+#importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#importing dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+
+"""classifying dependent and independent variables
+   here country, age and salary are independent variables
+   where as purchased is dependent"""
+
+X = dataset.iloc[:,2:4].values  
+Y = dataset.iloc[:,4].values  
+
+"""
+#Dealing with missing data
+
+from sklearn.preprocessing import Imputer
+
+imputer = Imputer(missing_values='NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
+#df_X = pd.DataFrame(X) 
+
+
+#Dealing with categorical data
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+labelencoder_Y = LabelEncoder()
+Y=labelencoder_Y.fit_transform(Y)
+"""
+
+
+#splitting into training set and test set
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train,Y_test = train_test_split(X,Y,test_size = 0.25, random_state=0)
+
+
+#Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test) 
+
+
+
+######################################## Logistic Regression ######################################
+
+#1 Create a classifier and provide the training set
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train,Y_train)
+
+
+#predicting the results for test set
+Y_pred = classifier.predict(X_test)
+
+# confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test,Y_pred)
+
+# Visualising the model for training set
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_train,Y_train
+
+X1,X2 = np.meshgrid(np.arange(start = X_set[:,0].min() - 1 ,stop = X_set[:,0].max() + 1, step = 0.01),
+                    np.arange(start = X_set[:,1].min() - 1 ,stop = X_set[:,1].max() + 1, step = 0.01))
+
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.50, cmap = ListedColormap(('red','green')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+
+for i,j in enumerate(np.unique(Y_set)):
+     plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c = ListedColormap(('red','green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+# Visualising the model for test set
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_test,Y_test
+
+X1,X2 = np.meshgrid(np.arange(start = X_set[:,0].min() - 1 ,stop = X_set[:,0].max() + 1, step = 0.001),
+                    np.arange(start = X_set[:,1].min() - 1 ,stop = X_set[:,1].max() + 1, step = 0.001))
+
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.50, cmap = ListedColormap(('red','green')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+
+for i,j in enumerate(np.unique(Y_set)):
+     plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c = ListedColormap(('red','green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
